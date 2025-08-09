2f89e8300b6a0323055030355a774ca18765bab4
3xau1o <19735362+3xau1o@users.noreply.github.com>
2025-08-08 22:10:00 -0700

    SDL_CreateTexture

--- Sources/SDL3/sdl3-img.h
+++ Sources/SDL3/sdl3-img.h
@@ -1,4 +1,7 @@
 #include "SDL3_image/SDL_image.h"
 
+/** https://wiki.libsdl.org/SDL3/SDL_CreateTexture */
+SDL_Texture *SDL_CreateTexture(SDL_Renderer _Nonnull *renderer, SDL_PixelFormat format, SDL_TextureAccess access, int w, int h);
+
 /** https://wiki.libsdl.org/SDL3_image/IMG_LoadTexture */
 SDL_Texture *IMG_LoadTexture(SDL_Renderer _Nonnull *renderer, const char _Nonnull *file);
